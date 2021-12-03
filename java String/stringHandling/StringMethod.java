86a7f7486476b4255f78197d0ceec6e3645944dd
Anil Bishnoi <bishnoianilakb@gmail.com>
2021-12-03 14:32:53 +0530

    collection Framework

--- java String/stringHandling/StringMethod.java
+++ java String/stringHandling/StringMethod.java
@@ -2,11 +2,13 @@ package stringHandling;
 
 public class StringMethod {
     // string class methods 
-    // length()   isEmpty()   trim()    equal()   equalsIgnoreCase()    compareTo()   
+    // length()   isEmpty()   trim()    compareTo()   
+    // equal()   {upper & lower Case checked}
+    // equalsIgnoreCase()   {upper & lower Case Ignored}
     // compareToIgnoreCase()
-public static void main(String[] args) {
-    String sEqual1 = new String("Apple");
-    String sEqual2 = new String("Apple"); // uppCase and lowerCase matter
-    System.out.println("equal() ==> "+(sEqual1.equals(sEqual2)));
-}
+    public static void main(String[] args) {
+        String sEqual1 = new String("Apple");
+        String sEqual2 = new String("Apple"); // uppCase and lowerCase matter
+        System.out.println("equal() ==> "+(sEqual1.equals(sEqual2)));
+    }
 }
