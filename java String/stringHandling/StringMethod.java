e419e36c86bfa597ebee44e79a60efec31da86c4
Anil Bishnoi <bishnoianilakb@gmail.com>
2021-12-03 11:46:28 +0530

    string class methods

--- java String/stringHandling/StringMethod.java
+++ java String/stringHandling/StringMethod.java
@@ -1,5 +1,12 @@
 package stringHandling;
 
 public class StringMethod {
-    
+    // string class methods 
+    // length()   isEmpty()   trim()    equal()   equalsIgnoreCase()    compareTo()   
+    // compareToIgnoreCase()
+public static void main(String[] args) {
+    String sEqual1 = new String("Apple");
+    String sEqual2 = new String("Apple"); // uppCase and lowerCase matter
+    System.out.println("equal() ==> "+(sEqual1.equals(sEqual2)));
+}
 }
