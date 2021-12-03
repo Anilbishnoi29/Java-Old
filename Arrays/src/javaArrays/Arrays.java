b64c8ef9dacb50305616cab5c2a59db769c74253
Anil Bishnoi <bishnoianilakb@gmail.com>
2021-12-03 21:04:13 +0530

    new setting

--- Arrays/src/javaArrays/Arrays.java
+++ Arrays/src/javaArrays/Arrays.java
@@ -1,6 +1,11 @@
 package javaArrays;
+
 public class Arrays {
     public static void main(String[] args) {
-        System.out.println("hsdfg");
+        String[] intArray = {"h","e","l","l","o"};
+        for (String num : intArray) {
+    // only changes num, not the array element
+        System.out.print(num);
+        }
     }
 }
