c01fcf3aeddb6630a9840cc04d601a29c3a475c3
Anil Bishnoi <bishnoianilakb@gmail.com>
2021-12-04 22:19:26 +0530

    Change Java Folder to imp Java

---
+++ Basic Java/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'a';
+        byte by = 10;
+        short sh = 10000;
+        int i = 1000000;
+        long l = 10000000L;
+        float f1 = 234.5f;
+        double d = 12.3;
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f1);
+        System.out.println(d);
+
+    }
+}
